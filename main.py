132bae633cca63bc210c025502c6e4715c34a4d3
Samitalimbu <Samitalimbu396@gmail.com>
2021-05-30 11:57:22 +0545

    Uploading complete project

--- main.py
+++ main.py
@@ -141,6 +141,133 @@ def register_screen():
     Button(register_window, text="Login", bg="#F0F0F0", border=0, fg="blue", command=login_screen).place(x=145, y=430)
 
 
+def home_screen():
+    window.deiconify()
+    window.geometry("300x600")
+    window.geometry("+550+50")
+    window.resizable(False, False)
+    window.title("Todo App")
+    login_window.withdraw()
+
+    global header, add
+    add = ImageTk.PhotoImage(Image.open("./images/plus.png"))
+    header = ImageTk.PhotoImage(Image.open("./images/header.png"))
+    Label(window, image=header).place(x=0, y=0, width=300, height=200)
+
+    today = date.today()
+    today = today.strftime("%B %d, %Y")
+
+    name = db.get_name(uid)
+    lbl_name = Label(window, text=name, font=("Arial", 12, "normal"), bg="black", fg="white")
+    lbl_name.place(x=10, y=150)
+    Label(window, text=today, bg="black", fg="white").place(x=10, y=170)
+    Button(window, image=add, border=0, command=lambda: add_task_screen(-1)).place(x=250, y=150)
+
+    y_axis = 0
+
+    for row in db.get_all_task(uid):
+        tsk = widget.CustomWidget(content_frame, row[2], row[0], row[3], row[5], check, circle)
+        tsk.bind("<Button-1>", lambda event, pos=row[0]: click_task(pos))
+        tsk.place(x=0, y=y_axis, height=50, width=300)
+        y_axis += 52
+
+
+def add_task_screen(task_id):
+    window.withdraw()
+    add_task_window.deiconify()
+    add_task_window.geometry("300x320")
+    add_task_window.geometry("+550+130")
+    add_task_window.resizable(False, False)
+    add_task_window.title("Add New Task")
+    global title_icon, category_icon, desc_icon
+    title_icon = ImageTk.PhotoImage(Image.open("./images/title.png"))
+    category_icon = ImageTk.PhotoImage(Image.open("./images/category.png"))
+    desc_icon = ImageTk.PhotoImage(Image.open("./images/description.png"))
+
+    ttk.Style().configure('pad.TEntry', padding='25 1 1 1')
+
+    Label(add_task_window, text="Title").place(x=10, y=10)
+    title = Entry(add_task_window, borderwidth=0)
+    title.place(x=40, y=30, height=25, width=250)
+    Label(add_task_window, image=title_icon, bg="white").place(x=10, y=30, width=30, height=25)
+
+    Label(add_task_window, text="Category").place(x=10, y=65)
+    category = Entry(add_task_window, borderwidth=0)
+    category.place(x=40, y=85, height=25, width=250)
+    Label(add_task_window, image=category_icon, bg="white").place(x=10, y=85, width=30, height=25)
+
+    Label(add_task_window, text="Description").place(x=10, y=115)
+    desc = Entry(add_task_window, borderwidth=0)
+    desc.place(x=40, y=135, height=60, width=250)
+    Label(add_task_window, image=desc_icon, bg="white").place(x=10, y=135, width=30, height=60)
+
+    def change():
+        var1.set(1) if var1.get() == 1 else var1.set(0)
+
+    def on_closing():
+        add_task_window.destroy()
+        window.deiconify()
+
+    def add_task():
+        db.add_task(title.get(), category.get(), desc.get(), var1.get(), uid)
+        messagebox.showinfo("Success", "Task added successfully!!!")
+        add_task_window.withdraw()
+        home_screen()
+
+    def update_task():
+        if db.update_task(task_id, title.get(), category.get(), desc.get(), var1.get()):
+            messagebox.showinfo("Success", "Task updated successfully!!!")
+            add_task_window.withdraw()
+            home_screen()
+        else:
+            messagebox.showerror("Failed", "Failed to update task")
+
+    def delete_task():
+        if db.delete_task(task_id):
+            messagebox.showinfo("Success", "Task deleted successfully!!!")
+            add_task_window.withdraw()
+
+            global content_frame
+            for w in content_frame.winfo_children():
+                w.destroy()
+
+            y_axis = 0
+
+            for row in db.get_all_task(uid):
+                tsk = widget.CustomWidget(content_frame, row[2], row[0], row[3], row[5], check, circle)
+                tsk.bind("<Button-1>", lambda event, pos=row[0]: click_task(pos))
+                tsk.place(x=0, y=y_axis, height=50, width=300)
+                y_axis += 52
+
+            home_screen()
+        else:
+            messagebox.showerror("Failed", "Failed to update task")
+
+    add_task_window.protocol("WM_DELETE_WINDOW", on_closing)
+
+    var1 = IntVar()
+    chk = Checkbutton(add_task_window, text='Completed', variable=var1, onvalue=1, offvalue=0, command=change)
+
+    global img_btn
+    img_btn = ImageTk.PhotoImage(Image.open("./images/btn_long.png"))
+    Button(add_task_window, text="Update" if task_id > 0 else "Save", border=0, fg="white", image=img_btn,
+           compound="center", command=update_task if task_id > 0 else add_task) \
+        .place(x=10, y=230, height=50, width=280)
+
+    if task_id > 0:
+        chk.place(x=10, y=200)
+        Button(add_task_window, text="Delete", border=0, fg="white", image=img_btn,
+               compound="center", command=delete_task) \
+            .place(x=10, y=270, height=50, width=280)
+        for row in db.get_task(task_id):
+            title.insert(0, row[2])
+            category.insert(0, row[3])
+            desc.insert(0, row[4])
+            var1.set(row[5])
+
+
+db.create_table()
+
 login_screen()
 register_window.withdraw()
 add_task_window.withdraw()
